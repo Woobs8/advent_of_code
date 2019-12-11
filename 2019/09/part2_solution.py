1404331556ccf652d7284c1fa2a33e7a601131c0
Woobs <mikku_pedersen@hotmail.com>
2019-12-11 20:10:20 +0100

    reverted accidental edit of day 9 part 2

--- 2019/09/part2_solution.py
+++ 2019/09/part2_solution.py
@@ -2,41 +2,11 @@ import argparse
 import sys
 sys.path.append('..')
 from Intcode.IntcodeComputer import IntcodeComputer
-from collections import defaultdict
 
-
-# represenations of panel colors
-BLACK = '.'
-WHITE = '#'
-
-
-# values representing the panel colors
-COLOR_CODES = {
-    BLACK:0,
-    WHITE:1
-}
 
 def read_from_file(fp:str) -> list:
     with open(fp, 'r') as f:
         return list(map(int, f.read().split(',')))
-
-
-def hull_painting_robot(memory:list):
-    program = IntcodeComputer(memory)
-    hull = defaultdict(lambda:BLACK)
-    painted = defaultdict(lambda:0)
-
-    res = IntcodeComputer.OUTPUT_CODES.OK
-    loc = (0,0)
-    while res != IntcodeComputer.OUTPUT_CODES.HALTED:
-        panel_color = hull[loc]
-        out = []
-        res = program.execute([COLOR_CODES[panel_color]], out)
-
-        hull[loc] = COLOR_CODES[out[0]]
-
-
-
 
 
 def BOOST_program(memory:list) -> int:
